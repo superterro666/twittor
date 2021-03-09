3d30a9d5d4e51a7fd05bc33edb5257a20b258d5d
jose campos <josecamposdeveloper@gmail.com>
2021-03-09 14:11:54 +0100

    error 3

--- bd/chequeoYaExisteUsuario.go
+++ bd/chequeoYaExisteUsuario.go
@@ -14,7 +14,7 @@ func ChequeoYaExisteUsuario(email string) (models.Usuario, bool, string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	db := MongoCN.Database("twittor")
+	db := MongoCN.Database("twitter")
 	col := db.Collection("usuarios")
 
 	condicion := bson.M{"email": email}
